2501f3643f444114fa12a121c8d685140d542a89
tlt.programmer <RURUKOH@RU-L-7347992>
2022-10-29 16:25:37 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace RGB_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbR = new System.Windows.Forms.TextBox();
+            this.tbG = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbHexColor = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbR
+            // 
+            this.tbR.Location = new System.Drawing.Point(245, 12);
+            this.tbR.MaxLength = 3;
+            this.tbR.Name = "tbR";
+            this.tbR.Size = new System.Drawing.Size(35, 22);
+            this.tbR.TabIndex = 0;
+            this.tbR.Text = "255";
+            this.tbR.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbR_MouseClick);
+            this.tbR.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbR_KeyPress);
+            this.tbR.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbR_KeyUp);
+            // 
+            // tbG
+            // 
+            this.tbG.Location = new System.Drawing.Point(245, 40);
+            this.tbG.MaxLength = 3;
+            this.tbG.Name = "tbG";
+            this.tbG.Size = new System.Drawing.Size(35, 22);
+            this.tbG.TabIndex = 0;
+            this.tbG.Text = "0";
+            this.tbG.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbG_MouseClick);
+            this.tbG.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbG_KeyPress);
+            this.tbG.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbG_KeyUp);
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(245, 68);
+            this.tbB.MaxLength = 3;
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(35, 22);
+            this.tbB.TabIndex = 0;
+            this.tbB.Text = "0";
+            this.tbB.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbB_MouseClick);
+            this.tbB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbB_KeyPress);
+            this.tbB.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbB_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(221, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(220, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(222, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "B";
+            // 
+            // tbHexColor
+            // 
+            this.tbHexColor.Location = new System.Drawing.Point(245, 110);
+            this.tbHexColor.Name = "tbHexColor";
+            this.tbHexColor.Size = new System.Drawing.Size(83, 22);
+            this.tbHexColor.TabIndex = 0;
+            this.tbHexColor.Text = "0";
+            this.tbHexColor.TextChanged += new System.EventHandler(this.tbHexColor_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(178, 120);
+            this.panel1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "HEX";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(286, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "#FF";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(286, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "#FF";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(286, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 17);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "#FF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(345, 142);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbHexColor);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.tbG);
+            this.Controls.Add(this.tbR);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RGB converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbR;
+        private System.Windows.Forms.TextBox tbG;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbHexColor;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGB_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            RGBtoColor(Convert.ToInt32(tbR.Text), Convert.ToInt32(tbG.Text), Convert.ToInt32(tbB.Text));
+        }
+
+        // Получение строки цвета в HEX виде
+        private static String ToHexConverter(System.Drawing.Color c)
+        {
+            return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+        }
+
+        private static String RGBConverter(System.Drawing.Color c)
+        {
+            return "RGB(" + c.R.ToString() + "," + c.G.ToString() + "," + c.B.ToString() + ")";
+        }
+
+        private void tbR_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyNumberInput(e); // Ввод только цифр 0....9
+        }
+
+        private void onlyNumberInput(KeyPressEventArgs e)
+        {
+            // 48 - это 0
+            // 49 - это 1
+            // 50 - это 2
+            // 51 - это 3
+            // 52 - это 4
+            // 53 - это 5
+            // 54 - это 6
+            // 55 - это 7
+            // 56 - это 8
+            // 57 - это 9
+
+            char number = e.KeyChar;
+            if ((number <= 7 || number >= 9) && (number <= 47 || number >= 58)) //цифры
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbG_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyNumberInput(e); // Ввод только цифр 0....9
+        }
+
+        private void tbB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyNumberInput(e); // Ввод только цифр 0....9
+        }
+
+        private void tbR_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Convert.ToInt32(tbR.Text) >= 255) tbR.Text = "255";
+            int num = Convert.ToInt32(tbR.Text);
+
+            RGBtoColor(num, Convert.ToInt32(tbG.Text), Convert.ToInt32(tbB.Text));
+        }
+
+        private void tbG_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Convert.ToInt32(tbG.Text) >= 255) tbG.Text = "255";
+            int num = Convert.ToInt32(tbG.Text);
+
+            RGBtoColor(Convert.ToInt32(tbR.Text), num, Convert.ToInt32(tbB.Text));
+        }
+
+        private void tbB_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Convert.ToInt32(tbB.Text) >= 255) tbB.Text = "255";
+            int num = Convert.ToInt32(tbB.Text);
+
+            RGBtoColor(Convert.ToInt32(tbR.Text), Convert.ToInt32(tbG.Text), num);
+        }
+
+        private void RGBtoColor(int r, int g, int b)
+        {
+            Color col = Color.FromArgb(255, r, g, b);
+
+            tbHexColor.Text = ToHexConverter(col);
+
+            panel1.BackColor = col;
+
+            label5.Text = r.ToString("X2");
+            label6.Text = g.ToString("X2");
+            label7.Text = b.ToString("X2");
+        }
+
+        private void tbR_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                tbR.SelectionStart = 0;
+                tbR.SelectionLength = tbR.Text.Length;
+                tbR.Focus();
+            }
+        }
+
+        private void tbG_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                tbG.SelectionStart = 0;
+                tbG.SelectionLength = tbG.Text.Length;
+                tbG.Focus();
+            }
+        }
+
+        private void tbB_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                tbB.SelectionStart = 0;
+                tbB.SelectionLength = tbB.Text.Length;
+                tbB.Focus();
+            }
+        }
+
+        private void tbHexColor_TextChanged(object sender, EventArgs e)
+        {
+            var color = System.Drawing.ColorTranslator.FromHtml(tbHexColor.Text);
+            tbR.Text = color.R.ToString();
+            tbG.Text = color.G.ToString();
+            tbB.Text = color.B.ToString();
+            panel1.BackColor = Color.FromArgb(255, color.R, color.G, color.B);
+
+            label5.Text = color.R.ToString("X2");
+            label6.Text = color.G.ToString("X2");
+            label7.Text = color.B.ToString("X2");
+        }
+    }
+}
